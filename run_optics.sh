76bc52646aa5c279201dee079aa4e94929fa2725
swardiantara <swardyantara@gmail.com>
2024-06-26 20:39:30 +0700

    birch and optics scenarios

---
+++ run_optics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+embeddings=( sbert instructor-base instructor-large instructor-xl )
+thresholds=( 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 )
+for embedding in "${embeddings[@]}"; do
+    for threshold in "${thresholds[@]}"; do
+        python optics.py --embedding "$embedding" --threshold "$threshold"
+    done
+done
