be2489e8e889879cb5b9a51f2f3a1c600ff57a1a
swardiantara <swardyantara@gmail.com>
2024-07-23 18:58:45 +0700

    switch env for certain scenarios

--- run_hdbscan.sh
+++ run_hdbscan.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-embeddings=( drone-sbert sbert simcse instructor-base instructor-large instructor-xl )
+conda activate tencon2
+
+embeddings=( drone-sbert sbert simcse )
+thresholds=( 0.3 0.29 0.28 0.27 0.26 0.25 0.24 0.23 0.22 0.21 )
+for embedding in "${embeddings[@]}"; do
+    for threshold in "${thresholds[@]}"; do
+        python hdbscan_log.py --embedding "$embedding" --threshold "$threshold" --output_dir hdbscan-ajk
+    done
+done
+
+conda activate tencon
+
+embeddings=( instructor-base instructor-large instructor-xl )
 thresholds=( 0.3 0.29 0.28 0.27 0.26 0.25 0.24 0.23 0.22 0.21 )
 for embedding in "${embeddings[@]}"; do
     for threshold in "${thresholds[@]}"; do
