87ea74a0153694f2e72d5e78693d81880d0f535e
swardiantara <swardyantara@gmail.com>
2024-07-23 19:08:28 +0700

    fix bash for switching conda env

--- run_dbscan.sh
+++ run_dbscan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-conda activate tencon2
+source /home/swardi/miniconda3/bin/activate tencon2
 
 embeddings=( drone-sbert sbert simcse )
 thresholds=( 0.3 0.29 0.28 0.27 0.26 0.25 0.24 0.23 0.22 0.21 0.2 0.19 0.18 0.17 0.16 0.15 0.14 0.13 0.12 0.11 0.1 0.09 0.08 0.07 0.06 0.05 0.04 0.03 0.02 0.01 )
@@ -10,7 +9,7 @@ for embedding in "${embeddings[@]}"; do
     done
 done
 
-conda activate tencon
+source /home/swardi/miniconda3/bin/activate tencon
 
 embeddings=( instructor-base instructor-large instructor-xl )
 thresholds=( 0.3 0.29 0.28 0.27 0.26 0.25 0.24 0.23 0.22 0.21 0.2 0.19 0.18 0.17 0.16 0.15 0.14 0.13 0.12 0.11 0.1 0.09 0.08 0.07 0.06 0.05 0.04 0.03 0.02 0.01 )
