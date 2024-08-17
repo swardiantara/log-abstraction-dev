c627db0ac4025b58297ef45aef13f2fe55f17ea4
swardiantara <swardyantara@gmail.com>
2024-08-17 17:10:41 +0700

    run birch on AJK

--- run_birch.sh
+++ run_birch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 embeddings=( drone-sbert sbert simcse instructor-base instructor-large instructor-xl )
-thresholds=( 0.3 0.29 0.28 0.27 0.26 0.25 0.24 0.23 0.22 0.21 )
-for embedding in "${embeddings[@]}"; do
-    for threshold in "${thresholds[@]}"; do
-        python birch.py --embedding "$embedding" --threshold "$threshold" --output_dir birch-ajk
+datasets=( drone apache )
+thresholds=( 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.2 0.21 0.22 0.23 0.24 0.25 0.26 0.27 0.28 0.29 0.3 )
+for dataset in "${datasets[@]}"; do
+    for embedding in "${embeddings[@]}"; do
+        for threshold in "${thresholds[@]}"; do
+            python birch.py --dataset "$dataset" --embedding "$embedding" --threshold "$threshold" --output_dir birch-new
+        done
     done
 done
--- run_threshold.sh
+++ run_threshold.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
 thresholds=( 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 )
+embeddings=( drone-sbert sbert simcse instructor-base instructor-large instructor-xl )
 
-for threshold in "${thresholds[@]}"; do
-    python agglomerative.py --embedding drone-sbert --linkage average --threshold "$threshold" --output_dir threshold_agglomerative
-done
-
-for threshold in "${thresholds[@]}"; do
-    python birch.py --embedding drone-sbert --threshold "$threshold" --output_dir threshold_birch
-done
-
-for threshold in "${thresholds[@]}"; do
-    python dbscan.py --embedding drone-sbert --threshold "$threshold" --output_dir threshold_dbscan
-done
-
-for threshold in "${thresholds[@]}"; do
-    python hdbscan_log.py --embedding drone-sbert --threshold "$threshold" --output_dir threshold_hdbscan
-done
-
-for threshold in "${thresholds[@]}"; do
-    python optics.py --embedding drone-sbert --threshold "$threshold" --output_dir threshold_optics
+for embedding in "${embeddings[@]}"; do
+    for threshold in "${thresholds[@]}"; do
+        python birch.py --embedding "$embedding" --threshold "$threshold" --output_dir threshold-analysis
+    done
 done
