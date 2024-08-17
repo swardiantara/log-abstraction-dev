e0a0361edc6b527eeeab5888962c8a0fa0117e36
swardiantara <swardyantara@gmail.com>
2024-08-17 21:21:10 +0700

    run birch on all datasets

--- birch.py
+++ birch.py
@@ -22,7 +22,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--output_dir', type=str, default='birch',
                     help="Folder to store the experimental results. Default: birch")
-parser.add_argument('--dataset', choices=['apache', 'drone'], default='drone',
+parser.add_argument('--dataset', choices=['Apache', 'drone', 'drone_ovs', 'Android', 'BGL', 'Hadoop', 'HDFS', 'HealthApp', 'HPC', 'Linux', 'Mac', 'OpenSSH', 'OpenStack', 'Proxifier', 'Spark', 'Thunderbird', 'Windows', 'Zookeeper'], default='drone',
                     help="Dataset to test. Default: drone")
 parser.add_argument('--embedding', choices=['sbert', 'instructor-base', 'instructor-large', 'instructor-xl', 'drone-sbert', 'simcse'], default='sbert',
                     help="Embedding model to extract the log's feature. Default: sbert")
@@ -39,10 +39,14 @@ def main():
     if args.dataset == 'drone':
         dataset = pd.read_excel(os.path.join('dataset', 'cluster_label.xlsx')).sort_values(by='message').reset_index(drop=True)
         labels_true = dataset['cluster_id'].to_list()
-    elif args.dataset == 'apache':
-        dataset = pd.read_csv(os.path.join('dataset', 'Apache_2k.log_structured.csv')).sort_values(by='LineId').reset_index(drop=True)
+    elif args.dataset == 'drone_ovs':
+        dataset = pd.read_excel(os.path.join('dataset', 'cluster_label_oversampled.xlsx')).sort_values(by='message').reset_index(drop=True)
+        labels_true = dataset['cluster_id'].to_list()
+    else:
+        dataset = pd.read_csv(os.path.join('dataset', f'{args.dataset}_2k.log_structured.csv')).sort_values(by='Content').reset_index(drop=True)
         dataset.rename(columns = {'Content': 'message', 'EventId': 'cluster_id'}, inplace = True)
         labels_true = dataset['cluster_id'].to_list()
+
 
     workdir = os.path.join('experiments', args.output_dir, args.dataset, args.embedding, str(args.threshold))
     print(f"[birch] - Current scenario: {workdir}")
@@ -65,17 +69,17 @@ def main():
         # Save the model to a file
         dump(clustering_model, os.path.join(workdir, 'birch_model.joblib'))
     
-    cluster_label_df = get_pred_df(clustering_model.labels_, dataset)
-    eval_score = evaluation_score(corpus_embeddings, dataset, cluster_label_df)
+    pred_df = get_pred_df(clustering_model.labels_, dataset)
+    eval_score = evaluation_score(corpus_embeddings, dataset, pred_df)
     
     duration = ended_at - started_at
     arguments_dict = vars(args)
     arguments_dict['started_at'] = str(started_at)
     arguments_dict['ended_at'] = str(ended_at)
-    arguments_dict['duration'] = str(duration.total_seconds()) + ' seconds'
+    arguments_dict['duration'] = str(round(duration.total_seconds() * 1000), 5) + ' miliseconds'
     arguments_dict['eval_score'] = eval_score
 
-    save_results(arguments_dict, cluster_label_df, workdir)
+    save_results(arguments_dict, pred_df, workdir)
 
     return 0
 
--- run_birch.sh
+++ run_birch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 embeddings=( drone-sbert sbert simcse instructor-base instructor-large instructor-xl )
-datasets=( drone apache )
+datasets=( drone apache drone_ovs Android BGL Hadoop HDFS HealthApp HPC Linux Mac OpenSSH OpenStack Proxifier Spark Thunderbird Windows Zookeeper )
 thresholds=( 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.2 0.21 0.22 0.23 0.24 0.25 0.26 0.27 0.28 0.29 0.3 )
 for dataset in "${datasets[@]}"; do
     for embedding in "${embeddings[@]}"; do
