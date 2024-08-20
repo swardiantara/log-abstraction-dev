e85c8fc6c4286d905db321b028bba903fa17460b
swardiantara <swardyantara@gmail.com>
2024-08-20 15:12:18 +0700

    initial threshold analysis

--- run_threshold.sh
+++ run_threshold.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+source /home/swardi/miniconda3/bin/activate tencon2
+
 thresholds=( 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 )
-embeddings=( drone-sbert sbert simcse instructor-base instructor-large instructor-xl )
+embeddings=( simcse )
 
 for embedding in "${embeddings[@]}"; do
     for threshold in "${thresholds[@]}"; do
-        python birch.py --embedding "$embedding" --threshold "$threshold" --output_dir threshold-analysis
+        python birch.py --embedding "$embedding" --threshold "$threshold" --output_dir threshold_birch
+    done
+done
+
+source /home/swardi/miniconda3/bin/activate tencon
+
+thresholds=( 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 )
+embeddings=( instructor-base instructor-large instructor-xl )
+
+for embedding in "${embeddings[@]}"; do
+    for threshold in "${thresholds[@]}"; do
+        python birch.py --embedding "$embedding" --threshold "$threshold" --output_dir threshold_birch
     done
 done
