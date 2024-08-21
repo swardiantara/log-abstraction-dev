93a5856379cf3cd8681bafb950190c1e989b9c19
swardiantara <swardyantara@gmail.com>
2024-08-22 03:01:08 +0700

    run agglomerative new

---
+++ agglomerative_new.py
@@ -0,0 +1,93 @@
+import os
+import pickle
+from joblib import dump
+import datetime
+import argparse
+import pandas as pd
+from sklearn.cluster import Birch
+from sklearn.cluster import AgglomerativeClustering
+from utils import get_features, get_pred_df, evaluation_score, compute_distance_matrix, save_results
+
+def restricted_float(x):
+    try:
+        x = float(x)
+    except ValueError:
+        raise argparse.ArgumentTypeError("%r not a floating-point literal" % (x,))
+
+    # if x < 0.01 or x > 0.2:
+    #     raise argparse.ArgumentTypeError("%r not in range [0.01, 0.2]"%(x,))
+    return x
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--output_dir', type=str, default='birch',
+                    help="Folder to store the experimental results. Default: birch")
+parser.add_argument('--dataset', choices=['Apache', 'drone', 'drone_ovs', 'Android', 'BGL', 'Hadoop', 'HDFS', 'HealthApp', 'HPC', 'Linux', 'Mac', 'OpenSSH', 'OpenStack', 'Proxifier', 'Spark', 'Thunderbird', 'Windows', 'Zookeeper'], default='drone',
+                    help="Dataset to test. Default: drone")
+parser.add_argument('--embedding', choices=['sbert', 'instructor-base', 'instructor-large', 'instructor-xl', 'drone-sbert', 'simcse'], default='sbert',
+                    help="Embedding model to extract the log's feature. Default: sbert")
+parser.add_argument('--threshold', type=restricted_float,
+                    help="Distance threshold for same cluster criteria [0.01,0.2]. Default: 0.07")
+parser.add_argument('--save_model', action='store_true',
+                    help="Wether to save the model for online prediction.")
+
+
+def main():
+    os.environ['TOKENIZERS_PARALLELISM'] = "false"
+    args = parser.parse_args()
+
+    if args.dataset == 'drone':
+        dataset = pd.read_excel(os.path.join('dataset', 'cluster_label.xlsx')).sort_values(by='message').reset_index(drop=True)
+        labels_true = dataset['cluster_id'].to_list()
+    elif args.dataset == 'drone_ovs':
+        dataset = pd.read_excel(os.path.join('dataset', 'cluster_label_oversampled.xlsx')).sort_values(by='message').reset_index(drop=True)
+        labels_true = dataset['cluster_id'].to_list()
+    else:
+        dataset = pd.read_csv(os.path.join('dataset', f'{args.dataset}_2k.log_structured.csv')).sort_values(by='Content').reset_index(drop=True)
+        dataset.rename(columns = {'Content': 'message', 'EventId': 'cluster_id'}, inplace = True)
+        labels_true = dataset['cluster_id'].to_list()
+
+
+    workdir = os.path.join('experiments', args.output_dir, args.dataset, args.embedding, str(args.threshold))
+    print(f"[birch] - Current scenario: {workdir}")
+    if os.path.exists(workdir) and not args.save_model:
+        if os.path.exists(os.path.join(workdir, 'scenario_arguments.json')):
+            print("[birch] - Scenario has been executed.")
+            return 0
+    elif not os.path.exists(workdir):
+        os.makedirs(workdir)
+
+    corpus_embeddings = get_features(dataset, args.embedding)
+    
+    clustering_model = Birch(threshold=args.threshold, n_clusters=None)
+    clustering_model = AgglomerativeClustering(n_clusters=None,
+                                    metric='precomputed',
+                                    linkage='average',
+                                    distance_threshold=args.threshold)
+    
+    started_at = datetime.datetime.now()
+    clustering_model.fit(corpus_embeddings)
+    ended_at = datetime.datetime.now()
+
+    if args.save_model:
+        # Save the model to a file
+        dump(clustering_model, os.path.join(workdir, 'birch_model.joblib'))
+    
+    pred_df = get_pred_df(clustering_model.labels_, dataset)
+    eval_score = evaluation_score(corpus_embeddings, dataset, pred_df)
+    
+    duration = ended_at - started_at
+    arguments_dict = vars(args)
+    arguments_dict['started_at'] = str(started_at)
+    arguments_dict['ended_at'] = str(ended_at)
+    arguments_dict['duration'] = str(round(duration.total_seconds() * 1000, 5)) + ' miliseconds'
+    arguments_dict['eval_score'] = eval_score
+
+    save_results(arguments_dict, pred_df, workdir)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
--- run_agglomerative.sh
+++ run_agglomerative.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-embeddings=( drone-sbert sbert instructor-base instructor-large instructor-xl )
-linkages=( complete average single )
-thresholds=( 0.3 0.29 0.28 0.27 0.26 0.25 0.24 0.23 0.22 0.21 )
+source /home/swardi/miniconda3/bin/activate tencon2
+
+
+embeddings=( drone-sbert sbert simcse )
+thresholds=( 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.2 0.21 0.22 0.23 0.24 0.25 0.26 0.27 0.28 0.29 0.3 )
 for embedding in "${embeddings[@]}"; do
-    for linkage in "${linkages[@]}"; do
-        for threshold in "${thresholds[@]}"; do
-            python agglomerative.py --embedding "$embedding" --linkage "$linkage" --threshold "$threshold" --output_dir agglomerative_ajk
-        done
+    for threshold in "${thresholds[@]}"; do
+        python agglomerative_new.py --embedding "$embedding" --threshold "$threshold" --output_dir agglomerative-new
+    done
+done
+
+
+source /home/swardi/miniconda3/bin/activate tencon
+embeddings=( instructor-base instructor-large instructor-xl )
+thresholds=( 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.2 0.21 0.22 0.23 0.24 0.25 0.26 0.27 0.28 0.29 0.3 )
+for embedding in "${embeddings[@]}"; do
+    for threshold in "${thresholds[@]}"; do
+        python agglomerative_new.py --embedding "$embedding" --threshold "$threshold" --output_dir agglomerative-new
     done
 done
